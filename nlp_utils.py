29a1a753d9b8c5ab1076689a8648cccfdb8a74db
Toby Cassidy <tobycassidy@Tobys-MacBook-Pro.local>
2021-10-13 07:28:01 +0100

    reformatting files with black

--- nlp_utils.py
+++ nlp_utils.py
@@ -1,45 +1,50 @@
-import re 
+import re
 import gensim
 import nltk
 import en_core_web_sm
 
-nltk.download('wordnet')
+nltk.download("wordnet")
 lemmatizer = nltk.stem.WordNetLemmatizer()
-spacy      = en_core_web_sm.load()
+spacy = en_core_web_sm.load()
 
 
 def case_normalize(text):
-    '''
+    """
     Takes in a text string and returns a string with all characters in lower case.
-    '''
+    """
     return text.lower()
 
+
 def remove_punctuation(text):
-    '''
+    """
     Takes in a text string and returns a string with all the punctuation removed.
-    '''
-    return re.sub('[^a-zA-Z0-9]', ' ', text)
+    """
+    return re.sub("[^a-zA-Z0-9]", " ", text)
+
 
 def tokenize(text):
-    '''
+    """
     Takes in a text string and returns a list where each item corresponds to a token.
-    '''
+    """
     return gensim.utils.tokenize(text)
 
+
 def lemmatize_nltk(text):
-    '''
+    """
     Takes in a list of tokens, lemmatizes each token according to nltk.stem.WordNetLemmatizer docs and returns text.
-    '''
-    return ' '.join([lemmatizer.lemmatize(word) for word in text])
+    """
+    return " ".join([lemmatizer.lemmatize(word) for word in text])
+
 
 def lemmatize_spacy(text):
-    '''
+    """
     Takes in text, lemmatizes each token according to en_core_web_sm.load docs and returns a list.
-    '''
+    """
     return [token.lemma_ for token in spacy(text)]
 
+
 def clean_text(text):
-    '''
+    """
     Takes in a raw text document and performs the following steps in order:
     - case normalization
     - punctuation removal
@@ -47,11 +52,11 @@ def clean_text(text):
     - lemmatization (nltk)
     - lemmatization (spacy)
     Then returns a string containing the processed text
-    '''
+    """
     text = case_normalize(text)
     text = remove_punctuation(text)
     text = tokenize(text)
     text = lemmatize_nltk(text)
     text = lemmatize_spacy(text)
-    text = ' '.join(text)
-    return text 
+    text = " ".join(text)
+    return text
--- process_data.py
+++ process_data.py
@@ -1,33 +1,48 @@
-import pandas as pd 
-import tensorflow as tf 
+import pandas as pd
+import tensorflow as tf
 import nlp_utils
 
 
+df_train = pd.read_csv("./assets/data/Corona_NLP_train.csv")
+df_test = pd.read_csv("./assets/data/Corona_NLP_test.csv")
 
-df_train = pd.read_csv('./assets/data/Corona_NLP_train.csv')
-df_test  = pd.read_csv('./assets/data/Corona_NLP_test.csv')
+df_train["TweetAt"] = [
+    date[-4:] + "-" + date[3:5] + "-" + date[0:2] for date in df_train["TweetAt"]
+]
+df_test["TweetAt"] = [
+    date[-4:] + "-" + date[3:5] + "-" + date[0:2] for date in df_test["TweetAt"]
+]
 
-df_train['TweetAt'] = [date[-4:] + '-' + date[3:5] + '-' + date[0:2] for date in df_train['TweetAt']]
-df_test['TweetAt']  = [date[-4:] + '-' + date[3:5] + '-' + date[0:2] for date in df_test['TweetAt']]
+df_train["sentiment_score"] = df_train["Sentiment"].replace(
+    {
+        "Extremely Negative": 1,
+        "Negative": 2,
+        "Neutral": 3,
+        "Positive": 4,
+        "Extremely Positive": 5,
+    }
+)
 
-df_train['sentiment_score'] = df_train['Sentiment'].replace({'Extremely Negative':1,
-                                                           'Negative':2,
-                                                           'Neutral':3,
-                                                           'Positive':4,
-                                                           'Extremely Positive':5})
-
-df_test['sentiment_score'] = df_test['Sentiment'].replace({'Extremely Negative':1,
-                                                           'Negative':2,
-                                                           'Neutral':3,
-                                                           'Positive':4,
-                                                           'Extremely Positive':5})
+df_test["sentiment_score"] = df_test["Sentiment"].replace(
+    {
+        "Extremely Negative": 1,
+        "Negative": 2,
+        "Neutral": 3,
+        "Positive": 4,
+        "Extremely Positive": 5,
+    }
+)
 
 # process text, this is a bottleneck so de-coupled from the app.py and stored processed data in separate files to save time on loading
-df_train['clean_tweet'] = df_train['OriginalTweet'].apply(lambda x: nlp_utils.clean_text(x))
-df_test['clean_tweet']  = df_test['OriginalTweet'].apply(lambda x: nlp_utils.clean_text(x))
+df_train["clean_tweet"] = df_train["OriginalTweet"].apply(
+    lambda x: nlp_utils.clean_text(x)
+)
+df_test["clean_tweet"] = df_test["OriginalTweet"].apply(
+    lambda x: nlp_utils.clean_text(x)
+)
 
 df_train.dropna(inplace=True)
 df_test.dropna(inplace=True)
 
-df_train.to_csv('./assets/data/processed_train.csv')
-df_test.to_csv('./assets/data/processed_test.csv')
+df_train.to_csv("./assets/data/processed_train.csv")
+df_test.to_csv("./assets/data/processed_test.csv")
